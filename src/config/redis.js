b0579989c7ddb4e19e1f1aaaeaf4d0de8b27fd64
Carlos Oliveira <54965836+burn-c@users.noreply.github.com>
2019-12-18 12:39:21 -0300

    Update redis.js

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,4 +1,7 @@
 export default {
-  host: 'localhost',
-  port: 6379
+  host: 'ec2-34-199-235-222.compute-1.amazonaws.com',
+  port: 20139,
+  user: 'h',
+  password: 'pb8769af3f6664537ed8c2942ee6b096db237caa18c9d55f03f8eb6752a29ad27'
+  
 };
