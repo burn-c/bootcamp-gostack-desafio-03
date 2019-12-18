a6f6dda426fcf0dd3d9a2bb528287d8fd3b03404
Carlos Oliveira <54965836+burn-c@users.noreply.github.com>
2019-12-18 09:53:15 -0300

    Update Postgres

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'gympoint',
+  host: 'ec2-54-221-244-70.compute-1.amazonaws.com',
+  username: 'kisvlaioyipqpy',
+  password: 'f59a8831fc49e79f5d5ee236d5a02dc6dafa1c954fc0f1f7319c8b28e660600f',
+  database: 'd3dj202d79hslf',
   define: {
     timestamps: true,
     underscored: true,
