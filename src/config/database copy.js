5067d8f753c5f9b382fbbc68a80b67b25716f517
burn-c <chopcsom@gmail.com>
2019-12-18 19:14:28 -0300

    Update: Database/Redis

--- src/config/database copy.js
+++ src/config/database copy.js
@@ -1,10 +1,8 @@
 module.exports = {
   dialect: 'postgres',
-  uri:
-    'postgres://kisvlaioyipqpy:f59a8831fc49e79f5d5ee236d5a02dc6dafa1c954fc0f1f7319c8b28e660600f@ec2-54-221-244-70.compute-1.amazonaws.com:5432/d3dj202d79hslf',
   host: 'ec2-54-221-244-70.compute-1.amazonaws.com',
   username: 'kisvlaioyipqpy',
-  // password: 'f59a8831fc49e79f5d5ee236d5a02dc6dafa1c954fc0f1f7319c8b28e660600f',
+  password: 'f59a8831fc49e79f5d5ee236d5a02dc6dafa1c954fc0f1f7319c8b28e660600f',
   database: 'd3dj202d79hslf',
   ssl: true
   // define: {
--- src/config/redis.js
+++ src/config/redis.js
@@ -3,5 +3,4 @@ export default {
   port: 20139,
   user: 'h',
   password: 'pb8769af3f6664537ed8c2942ee6b096db237caa18c9d55f03f8eb6752a29ad27'
-  
 };
